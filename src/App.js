6c3fe1ec6f043d169b6a2fd691cec645c02e5775
Eduardo Pedroso <drdpedroso@gmail.com>
2017-10-01 14:16:57 -0300

    Mounting map with search and recompose

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Map from './Map'
+import MapWithASearchBox from './Map'
 
 class App extends Component {
 
   componentDidMount () {
     fetch('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson')
     .then ((response) => {
-      debugger
       response.json().then((geoResponse) => {
         console.log(geoResponse)
       }).catch(err => console.log(err))
@@ -16,22 +15,16 @@ class App extends Component {
       console.log(error)
     })
   }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <Map 
-            isMarkerShown
-            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDr2XPy0JVf00LBuJzG0611ZreDARqIJs8&v=3.exp&libraries=geometry,drawing,places"
-            loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: `400px` }} />}
-            mapElement={<div style={{ height: `100%` }} />}
-          />
-        </p>
+        <MapWithASearchBox 
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDr2XPy0JVf00LBuJzG0611ZreDARqIJs8&v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
       </div>
     );
   }
